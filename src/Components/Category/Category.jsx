b5475c4fe4acd33ce2e8caf75e1b4de90be3934a
Martin Amankwaa <martyofmca@gmail.com>
2024-06-09 00:26:20 +0000

    feat: add category selected to application state

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
 import './Category.css';
 
-const Category = ({ icon, label, toolTip }) => {
+const Category = ({ icon, label, toolTip, trigger }) => {
+
+  const fireTrigger = () => {
+    trigger(label);
+  };
 
   if (!icon || !label) return;
   return (
-    <button aria-label={label === 'CC' ? 'Credit Cards' : label} title={toolTip} className="category flex gap-1 items-center justify-start w-full px-4 py-2 rounded-full">
+    <button aria-label={label === 'CC' ? 'Credit Cards' : label} title={toolTip} className="category flex gap-1 items-center justify-start w-full px-4 py-2 rounded-full" onClick={fireTrigger}>
         <span>{icon}</span>
         <span>{label}</span>
     </button>
@@ -16,6 +20,7 @@ Category.propTypes = {
   icon: PropTypes.element.isRequired,
   label: PropTypes.string.isRequired,
   toolTip: PropTypes.string.isRequired,
+  trigger: PropTypes.func.isRequired,
 };
 
 export default Category;
